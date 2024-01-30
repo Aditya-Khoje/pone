586ba6fadee25d78e0126efb7bfd81142a905da6
Aditya-Khoje <adityakhoje1@gmail.com>
2024-01-30 19:30:10 +0530

    4

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import First from "./Components/First.js";
 import Second from "./Components/Second.js";
+import Navbar from "./Components/Navbar.js";
 import "./Stylesheet/First.css";
 import { useState } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./Components/About.js";
+import Contact from "./Components/Contact.js";
+import Login from "./Components/Login.js";
 
 function App() {
   let first_prop = "Hii This is First Prop";
@@ -28,34 +34,53 @@ function App() {
 
   return (
     <>
-      <h1>Hi This is H1 Heading</h1>
-      <h2 className="display">
-        Display Board
-        <hr />
-        <b>{displayText}</b>
-      </h2>
+      <BrowserRouter>
+        <Navbar />
 
-      <div className="main-container-App">
-        <br />
-        <hr />
-        <br />
-        <div>
-          <h2>This is App Component</h2>
+        <h1>Hi This is H1 Heading</h1>
+        <h2 className="display">
+          Display Board
+          <hr />
+          <b>{displayText}</b>
+        </h2>
 
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={AppCompbtn1}
-          >
-            App Comp. btn1
-          </button>
+        <div className="main-container-App">
+          <br />
+          <hr />
+          <br />
+          <div>
+            <h2>This is App Component</h2>
+
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={AppCompbtn1}
+            >
+              App Comp. btn1
+            </button>
+          </div>
+          <br />
+          <hr />
+          <br />
         </div>
+        <First one={first_prop} FirstCompbtn1={FirstCompbtn1} />
+        <Second one={Second_prop} SecondCompbtn1={SecondCompbtn1} />
+        <br />
         <br />
         <hr />
+        <div className="break"></div>
+        <hr />
+        <hr />
+        <div className="break"></div>
+        <hr />
         <br />
-      </div>
-      <First one={first_prop} FirstCompbtn1={FirstCompbtn1} />
-      <Second one={Second_prop} SecondCompbtn1={SecondCompbtn1} />
+        <br />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
